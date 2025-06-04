d48eb3307ccf14cbf087b4e198965cd2ac2011a8
Mohammad Hasweh <hasweh03@gmail.com>
2025-06-05 00:31:31 +0300

    update redis connection method

--- src/config.py
+++ src/config.py
@@ -7,8 +7,7 @@ class Settings(BaseSettings):
     JWT_SECRET:str
     JWT_ALGORITHM:str
 
-    REDIS_HOST:str="localhost"
-    REDIS_PORT:int=6379
+    REDIS_URL: str = "redis://localhost:6379/0"
 
     MAIL_USERNAME:str
     MAIL_PASSWORD:str
--- src/db/redis.py
+++ src/db/redis.py
@@ -1,13 +1,10 @@
-import redis.asyncio as redis
+import redis.asyncio as aioredis
 from src.config import Config
 
 JTI_EXPIRY=3600
 
-token_blocklist=redis.Redis(
-    host=Config.REDIS_HOST,
-    port=Config.REDIS_PORT
-    ,db=0
-)
+token_blocklist=aioredis.from_url(Config.REDIS_URL)  # Connect using a URL
+
 
 
 async def add_jti_to_blocklist(jti:str)->None:
